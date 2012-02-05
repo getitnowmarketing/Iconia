f7adf27140963f7ccf047b9e9236b7b39fee5904
Your Name <getitnowmarketing@gmail.com>
2012-02-05 10:29:57 -0500

    If user space is waiting for a syncpt value higher than max,
    nvhost_wait_timeout() panics the kernel. This panic is based on the
    
    assumption that nobody should wait for sync points higher than indicated
    
    by a submit from user space.

--- drivers/video/tegra/host/nvhost_syncpt.c
+++ drivers/video/tegra/host/nvhost_syncpt.c
@@ -170,8 +170,9 @@ int nvhost_syncpt_wait_timeout(struct nvhost_syncpt *sp, u32 id,
 	if (value)
 		*value = 0;
 
-	BUG_ON(!check_max(sp, id, thresh));
-
+	if (!check_max(sp, id, thresh))
+              return -EINVAL;
+	
 	/* first check cache */
 	if (nvhost_syncpt_min_cmp(sp, id, thresh)) {
 		if (value)
